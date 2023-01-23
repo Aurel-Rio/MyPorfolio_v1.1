d69e0bf8b556c1be8d3924a7d21adf2c018fa95c
Aurel-Rio <servant.aurelien.developer@gmail.com>
2023-01-23 14:09:26 +0100

    MyPorfoliio_v1.1

--- utils/myDbConnect.php
+++ utils/myDbConnect.php
@@ -1,17 +1,19 @@
 <?php
 
 
-class  MyDbConfig {
+class  MyDbConfig
+{
     private $host = "127.0.0.1";
     private $dbName = "my_porfolio";
     private $userName = "riozacki";
     private $password = "";
     public $connect;
 
-    public  function getConnection() {
+    public  function getConnection()
+    {
         $this->connect = null;
         try {
-            $this->connect = new PDO("mysql:host=".$this->host."; dbname=".$this->dbName, $this->userName, $this->password);
+            $this->connect = new PDO("mysql:host=" . $this->host . "; dbname=" . $this->dbName, $this->userName, $this->password);
             $this->connect->exec("set names utf8");
             echo 'Connexion réussie';
             $forLog = 'Connexion réussie';
@@ -20,13 +22,9 @@ class  MyDbConfig {
             $forLogAndDate = "\n" . $forLog . " " . $date . " " . "LOL";
             error_log($forLogAndDate, 3, $logFile);
             print_r('connexion à la base de donées établie');
-        } catch(PDOException $exception) {
-            echo "Erreur de connexion".$exception->getMessage();
+        } catch (PDOException $exception) {
+            echo "Erreur de connexion" . $exception->getMessage();
         }
         return $this->connect;
     }
 }
-
-
-
-?>
